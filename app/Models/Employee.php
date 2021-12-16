d7497f1402278419b4e146d74d2261b5aaaa12cd
Julius Jr <kazibwejuliusjunior@gmail.com>
2021-12-16 04:20:34 +0300

    init

---
+++ app/Models/Employee.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name','dob','salary','department', 'cadre', 'job', 'salary',
+        'residence','gender','role'
+    ];
+}
